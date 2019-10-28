566ddbf19ac50fa20532f323ed75cbd0fd04a3ef
visgotti <=>
2019-10-27 18:24:14 -0600

    refactored some variables and handlers

--- src/Web.ts
+++ src/Web.ts
@@ -1,3 +1,5 @@
+import {GOTTI_ROUTE_BODY_PAYLOAD} from "./Protocol";
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -12,26 +14,46 @@ import { BaseWebServer } from "./WebServers/Base";
 
 export class WebServer extends BaseWebServer {
     public app: any;
-    private gate: GateWebServer;
-    private auth: Authentication;
+    public gate: GateWebServer;
+    public auth: Authentication;
     private isInitialized: boolean = false;
     private server: any;
-    readonly clientPath: string;
+    readonly publicPath: string;
+    readonly clientFilePath: string;
     readonly port: number;
 
-    constructor(port, clientPath) {
+    constructor(port, clientFilePath, publicContentPath) {
         super();
-        this.clientPath = clientPath;
+        this.publicPath = publicContentPath;
+        this.clientFilePath = clientFilePath;
         this.app = express();
         this.port = port;
     }
 
+    public addHandler(route: string, handler: Function) {
+        this.app.post(`/${route}`, async (req, res) => {
+            try {
+                const clientRequestOptions = req.body[GOTTI_ROUTE_BODY_PAYLOAD];
+                const response = await handler(clientRequestOptions);
+                return res.send(200).json(response);
+            } catch(err) {
+                const msg = err.message ? err.message : err;
+                return res.send(401).json(msg);
+            }
+        });
+    }
+
     public async init() {
         this.app.use(helmet());
-        this.app.use('/', express.static(this.clientPath));
+        this.app.use('/', express.static(this.publicPath));
         this.app.use(bodyParser.urlencoded({extended: true}));
         this.app.use(bodyParser.text());
         this.app.use(bodyParser.json({type: 'application/json'}));
+
+        this.app.get('/', (req, res) => {
+            res.sendFile(this.clientFilePath);
+        });
+
         return new Promise((resolve, reject) => {
             this.server = this.app.listen(this.port, () => {
                 this.isInitialized = true;
